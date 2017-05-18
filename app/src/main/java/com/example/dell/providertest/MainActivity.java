ace3b52a144065915a9c52551f5693b5594b4a16
kentanvictor <2030457926@qq.com>
2017-05-18 11:27:15 +0800

    second commit

--- app/src/main/java/com/example/dell/providertest/MainActivity.java
+++ app/src/main/java/com/example/dell/providertest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.dell.providertest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -34,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Uri uri = Uri.parse("content://com.example.dell.databasetest.provider/book");
+                Cursor cursor = getContentResolver().query(uri,null,null,null,null);
+                if(cursor != null)
+                {
+                    while (cursor.moveToNext())
+                    {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("MainActivity","book name is "+ name);
+                        Log.d("MainActivity","book author is "+ author);
+                        Log.d("MainActivity","book pages is "+ pages);
+                        Log.d("MainActivity","book price is "+ pages);
+                    }
+                    cursor.close();
+                }
             }
         });
     }
