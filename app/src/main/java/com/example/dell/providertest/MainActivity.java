4998f89566972b1c61b7255a319cc3ba6e00919c
kentanvictor <2030457926@qq.com>
2017-08-23 22:17:37 +0800

    forth commit

--- app/src/main/java/com/example/dell/providertest/MainActivity.java
+++ app/src/main/java/com/example/dell/providertest/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private String newId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +23,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Uri uri = Uri.parse("content://com.example.dell.databasetest.provider/book");//将一个内容URI解析成Uri对象
                 ContentValues values = new ContentValues();//把需要添加的数据都存放到ContentValues对象中
-                values.put("name","A Clash of Kings");
-                values.put("author","George Martin");
-                values.put("pages",1040);
-                values.put("price",22.85);
-                Uri newUri = getContentResolver().insert(uri,values);//调用ContentResolver的insert()方法执行添加操作
+                values.put("name", "A Clash of Kings");
+                values.put("author", "George Martin");
+                values.put("pages", 1040);
+                values.put("price", 22.85);
+                Uri newUri = getContentResolver().insert(uri, values);//调用ContentResolver的insert()方法执行添加操作
                 newId = newUri.getPathSegments().get(1);
                 /**
                  * 把insert()方法返回的返回的Uri对象中的新的id通过getPathSegments()方法将这个id取出
-                * */
+                 * */
             }
         });
         Button queryData = (Button) findViewById(R.id.query_data);
@@ -39,20 +40,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Uri uri = Uri.parse("content://com.example.dell.databasetest.provider/book");
-                Cursor cursor = getContentResolver().query(uri,null,null,null,null);
-                if(cursor != null)
-                {
-                    while (cursor.moveToNext())
-                    {
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
                         String name = cursor.getString(cursor.getColumnIndex("name"));
                         String author = cursor.getString(cursor.getColumnIndex("author"));
                         int pages = cursor.getInt(cursor.getColumnIndex("pages"));
                         double price = cursor.getDouble(cursor.getColumnIndex("price"));
-                        Log.d("MainActivity","book name is "+ name);
-                        Log.d("MainActivity","book author is "+ author);
-                        Log.d("MainActivity","book pages is "+ pages);
-                        Log.d("MainActivity","book price is "+ pages);
-                    }cursor.close();
+                        Log.d("MainActivity", "book name is " + name);
+                        Log.d("MainActivity", "book author is " + author);
+                        Log.d("MainActivity", "book pages is " + pages);
+                        Log.d("MainActivity", "book price is " + pages);
+                    }
+                    cursor.close();
                 }
             }
         });
@@ -62,11 +62,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Uri uri = Uri.parse("content://com.example.dell.databasetest.provider/book" + newId);
+                /**
+                 * 为了不想让Book表中其他行受影响，在调用parse()方法的时候，在后面添加一个新的id
+                 * 这个新的id就是添加数据
+                 * */
                 ContentValues values = new ContentValues();
-                values.put("name","A storm of Swords");
-                values.put("pages",1216);
-                values.put("price",24.05);
-                getContentResolver().update(uri,values,null,null);
+                values.put("name", "A storm of Swords");
+                values.put("pages", 1216);
+                values.put("price", 24.05);
+                getContentResolver().update(uri, values, null, null);
             }
         });
         Button deleteData = (Button) findViewById(R.id.delete_data);
@@ -75,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Uri uri = Uri.parse("content://com.example.dell.databasetest.provider/book" + newId);
-                getContentResolver().delete(uri,null,null);
+                getContentResolver().delete(uri, null, null);
             }
         });
     }
